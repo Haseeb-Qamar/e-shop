51b7e307471957889f7b9122f8acfb050a627da9
unknown <haseebqamar92@gmail.com>
2018-11-19 17:45:47 +0500

    added login page

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="bs/css/bootstrap.css">
+    <link rel="stylesheet" href="bs/js/bootstrap.js">
+    <link rel="stylesheet" href="jQ/jquery-3.3.1.js">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <div class="bgcontainer">
+    <div class="container">
+       
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="loginbox">
+<!--                   <h1 class="text-center" id="title">E - SHOP</h1>-->
+                    <div class="fields">
+                        <h1 class="text-center">Login</h1>
+                        <form action="">
+                        <span class="headings">User Name</span>
+                        <input type="text" required>
+                        <span class="headings">Password</span>
+                        <input type="password" required>
+                        <input type="submit" class="btn btn-outline-success" value="Login" id="loginbtn">
+                        </form>
+                    <a href="#">Create an account.</a>  
+                    </div>
+                      
+                </div>
+                
+            </div>
+        </div>
+    </div>   
+   </div>
+    
+</body>
+</html>
